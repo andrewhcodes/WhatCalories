784a34356710a7c4802cad8ba94de853a74d6d65
AndrewmhiOS <andrewmh.swift@gmail.com>
2023-05-28 14:49:08 -0400

    Initial Commit

---
+++ What Calories?/What_Calories_App.swift
@@ -0,0 +1,17 @@
+//
+//  What_Calories_App.swift
+//  What Calories?
+//
+//  Created by Andrew Hernandez on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct What_Calories_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
